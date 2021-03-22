a9c7892b02ebbbed8b7b17eeda79a738ced4691c
linyouquan <1994.3.2>
2021-03-22 23:50:33 +0800

    集成Druid数据库连接池和监控

--- src/main/java/com/terryxi/datacenter/DatacenterApplication.java
+++ src/main/java/com/terryxi/datacenter/DatacenterApplication.java
@@ -1,10 +1,11 @@
 package com.terryxi.datacenter;
 
+import com.alibaba.druid.spring.boot.autoconfigure.DruidDataSourceAutoConfigure;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = DruidDataSourceAutoConfigure.class)
 @MapperScan("com.terryxi.datacenter.mapper")
 public class DatacenterApplication {
 
