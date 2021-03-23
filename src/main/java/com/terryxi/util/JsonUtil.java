1ac3415623d65ecb2361e9b8ef6a25a67858b943
linyouquan <1994.3.2>
2021-03-23 22:53:41 +0800

    返回json数据中null字段不显示

--- src/main/java/com/terryxi/util/JsonUtil.java
+++ src/main/java/com/terryxi/util/JsonUtil.java
@@ -1,33 +1,29 @@
 package com.terryxi.util;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class JsonUtil {
-    /**
-     * 001.json转换成对象
-     * @param:传入对象，json字符串
-     * @return:Object
-     */
-    public static Object jsonToObj(Object obj,String jsonStr){
+
+    public static Object jsonToObj(Object obj, String jsonStr) {
         try {
             ObjectMapper mapper = new ObjectMapper();
             obj = mapper.readValue(jsonStr, obj.getClass());
-        }catch (Exception e){
+        } catch (Exception e) {
             return "Json转换错误!";
         }
         return obj;
     }
-    /**
-     * 002.对象转换成json
-     * @param:传入对象
-     * @return:json字符串
-     */
-    public static String objToJson(Object obj){
+
+
+    public static String objToJson(Object obj) {
         ObjectMapper mapper = new ObjectMapper();
+        mapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
         try {
             return mapper.writeValueAsString(obj);
-        }catch (Exception e){
-            return "Json转换错误";
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException("Json转换错误");
         }
     }
 }
